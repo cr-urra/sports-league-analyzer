03c3fb6e1a916ff8bdee97553ee9d260a60259fa
Cristóbal Urra <cristobal.urra@mail.udp.cl>
2021-12-22 15:50:01 -0300

    se añade implementacdion de ford fulkerson

--- grafo.py
+++ grafo.py
@@ -1,5 +1,6 @@
 import json
 import networkx as nx
+from collections import defaultdict
 
 # Algoritmo de flujo maximo con multiplicadores
 def flow_multiplier_ssp(G):
@@ -50,6 +51,63 @@ def create_b_graph(nombres, puntos=None, team_z=None):
         G.add_weighted_edges_from( [(nombres[i], 't', win_condition_capacity) ] )
 
     return G
+
+#implementación de ford_fulkerson
+
+class Graph:
+
+    def __init__(self, graph):
+        self.graph = graph
+        self. ROW = len(graph)
+
+
+    # Using BFS as a searching algorithm 
+    def searching_algo_BFS(self, s, t, parent):
+
+        visited = [False] * (self.ROW)
+        queue = []
+
+        queue.append(s)
+        visited[s] = True
+
+        while queue:
+
+            u = queue.pop(0)
+
+            for ind, val in enumerate(self.graph[u]):
+                if visited[ind] == False and val > 0:
+                    queue.append(ind)
+                    visited[ind] = True
+                    parent[ind] = u
+
+        return True if visited[t] else False
+
+    # Applying fordfulkerson algorithm
+    def ford_fulkerson(self, source, sink):
+        parent = [-1] * (self.ROW)
+        max_flow = 0
+
+        while self.searching_algo_BFS(source, sink, parent):
+
+            path_flow = float("Inf")
+            s = sink
+            while(s != source):
+                path_flow = min(path_flow, self.graph[parent[s]][s])
+                s = parent[s]
+
+            # Adding the path flows
+            max_flow += path_flow
+
+            # Updating the residual values of edges
+            v = sink
+            while(v != source):
+                u = parent[v]
+                self.graph[u][v] -= path_flow
+                self.graph[v][u] += path_flow
+                v = parent[v]
+
+        return max_flow
+
 
 # Funcion para crear grafo de futbol
 def create_f_graph(nombres, puntos=None, team_z=None):
